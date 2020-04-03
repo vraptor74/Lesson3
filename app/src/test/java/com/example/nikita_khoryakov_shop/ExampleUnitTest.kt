e7129091fb694c8e48864ba2373e88dfdbc8678f
vraptor74 <57489037+vraptor74@users.noreply.github.com>
2020-04-03 17:59:05 +0500

    added multiplication test

--- app/src/test/java/com/example/nikita_khoryakov_shop/ExampleUnitTest.kt
+++ app/src/test/java/com/example/nikita_khoryakov_shop/ExampleUnitTest.kt
@@ -2,9 +2,6 @@ package com.example.nikita_khoryakov_shop
 
 import org.junit.Test
 
-import org.junit.Assert.*
-import kotlin.math.sqrt
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -12,40 +9,60 @@ import kotlin.math.sqrt
  */
 class ExampleUnitTest {
 
-    fun PriceFormat(price : Double, measure: String, discount: Int = 0) : String{
-        val finalPrice : String
-        if((discount > 0) and (discount <= 100)){
-            //вычисляем цену по скидке
-            val discountPrice : Double = ((100 - discount)*price)/100;
-            //определяем, целое число или дробное
-            if (Math.floor(discountPrice) == (discountPrice)) {
-                //число целое - цена без дробной части
-                finalPrice  = Math.floor(discountPrice).toInt().toString()
-            } else {
-                //число дробное - цена с дробной частью
-                finalPrice = String.format("%.2f", discountPrice);
-            }
-            return "$finalPrice/$measure (скидка $discount%)"
-        }
-        else if (discount == 0){
-            //определяем, целое число или дробное
-            if (Math.floor(price) == (price)) {
-                //число целое - цена без дробной части
-                finalPrice  = Math.floor(price).toInt().toString()
-            } else {
-                //число дробное - цена с дробной частью
-                finalPrice = String.format("%.2f", price);
-            }
-            //выводим цену без скидки
-            return "$finalPrice/$measure"
-        }
-        else{
-            return "Скидка не может принимать значение меньше 0 или больше 100"
-        }
-    }
     @Test
-    fun addition_isCorrect() {
-        print(PriceFormat(8.0, "КГ", 4))
-    }
+    fun example() {
+
+        val iphoneCase = Product(price = 123.5, salePercent = 30)
+        val samsungCase = Product(price = 124.5, salePercent = 15)
+
+        val pricePrinter: PricePrinter = ConsolePricePrinter()
+
+        val discountIphoneCasePrice = iphoneCase.calcDiscountPrice()
+        pricePrinter.print(discountIphoneCasePrice)
+
+        val discountSamsungCasePrice = samsungCase.calcDiscountPrice()
+        pricePrinter.print(discountSamsungCasePrice)
 
     }
+}
+
+class Product(
+    /**
+     * Must be positive
+     */
+    private val price: Double,
+    private val salePercent: Int = 0
+) {
+    /**
+     * @return price with applied discount determined by [salePercent]
+     *
+     * If [salePercent] is more than 100 than product will have negative price
+     * If [salePercent] less than 0 product price will be increased
+     */
+    fun calcDiscountPrice(): Double = price * (1 - salePercent / 100.0)
+}
+
+interface PricePrinter {
+
+    /**
+     * Outputs price in <PRICE>P format.
+     * If price have not fractional part than it will be printed as integer
+     * If price have fractional part than it will be rounded for 2 symbols after "."
+     */
+    fun print(price: Double)
+}
+
+class ConsolePricePrinter : PricePrinter {
+
+    override fun print(discountPrice: Double) {
+
+        if (Math.floor(discountPrice) == (discountPrice)) {
+            println(Math.floor(discountPrice).toInt().toString())
+        } else {
+            println(String.format("%.2f", discountPrice))
+        }
+
+
+    }
+
+}
